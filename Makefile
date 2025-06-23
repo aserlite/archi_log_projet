91a76d80b8608e8ab0e971a1702204c6c6923f48
aserlite <arthurcuvillon691@gmail.com>
2025-06-23 11:03:53 +0200

    ajout .env et commande make init

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
-start:
+start up:
 	python3 -m venv venv && . venv/bin/activate && pip install -r requirements.txt || pip install flask flask-mysqldb flask-cors && python3 server.py
+
+init:
+	python3 -m venv venv && . venv/bin/activate && pip install --upgrade pip && pip install -r requirements.txt
+	@if [ ! -f .env ] && [ -f .env.example ]; then cp .env.example .env; fi
--- server.py
+++ server.py
@@ -1,15 +1,18 @@
-# server.py
 from flask import Flask, request, render_template, jsonify, redirect
 from flask_cors import CORS
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'flaskuser'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'flasktest'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app)
 
